e1c00a10732813883fa63bbcc80639ea66136bed
Jdzelhorst <jandouwezelhorst@gmail.com>
2023-01-16 20:27:06 +0000

    Add settings and playing board

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
-#Potential Imports
+# Imports
 from random import randint
 import os
 
-#Playing Board
-
-#Ships
-
 #Settings
+row_size = 9 
+col_size = 9 
+num_ships = 4 
+
+#Playing Board
+game_board = [["O"] * col_size for _ in range(row_size)]
+for board in game_board:
+    print(*board)
+#Ships
 
 #Game Start
 
