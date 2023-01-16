b299b2fd42f5f7224b02ef1944235b0e57465872
Jdzelhorst <jandouwezelhorst@gmail.com>
2023-01-16 18:35:08 +0000

    Add the main structure

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Potential Imports
+
+#Playing Board
+
+#Ships
+
+#Settings
+
+#Game Start
+
+#Game End
